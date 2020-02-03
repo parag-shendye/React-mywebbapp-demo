6283e029915a0392aaed08cda91ef7270c8cf2bd
paragshendye@gmail.com <paragshendye@gmail.com>
2020-02-03 20:24:01 +0100

    Updates

--- precache-manifest.8a0f35b39d0ec87989a6c35b355e2da2.js
+++ precache-manifest.8a0f35b39d0ec87989a6c35b355e2da2.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "00b9cf9c496a38605e53d7bcea5474f1",
+    "revision": "052b2104496c98fad5754e4ce7bc3c36",
     "url": "/React-mywebbapp-demo/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/React-mywebbapp-demo/static/css/2.d7476d31.chunk.css"
   },
   {
-    "revision": "6e2b60b5af0ca4c7f7da",
+    "revision": "83edd7b2a778f015eaa6",
     "url": "/React-mywebbapp-demo/static/css/main.4f13ce39.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/React-mywebbapp-demo/static/js/2.ae508f61.chunk.js.LICENSE"
   },
   {
-    "revision": "6e2b60b5af0ca4c7f7da",
-    "url": "/React-mywebbapp-demo/static/js/main.6f3f403a.chunk.js"
+    "revision": "83edd7b2a778f015eaa6",
+    "url": "/React-mywebbapp-demo/static/js/main.f45f712b.chunk.js"
   },
   {
     "revision": "30dd3da2d74435751f70",
